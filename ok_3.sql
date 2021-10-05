6044612cf0e0e56dc1595397eb63ed8a6506f93d
dmesitov <77168986+dmesitov@users.noreply.github.com>
2021-10-05 20:53:30 +0300

    Create ok_3.sql

---
+++ ok_3.sql
@@ -0,0 +1,27 @@
+select user_id,
+       item
+from (
+         select user_id,
+                item,
+                row_number() over (partition by user_id order by transaction_ts) as seq
+         from transactions
+     ) as sorted_aquisitions
+where seq = 1;
+
+
+--explain analyse verbose
+with earliest as (
+    select user_id,
+           min(transaction_ts) as earliest_order
+    from transactions
+    group by user_id)
+select sum(orders_3_days) / count(user_id) as average_per_user_3_days
+from (
+         select t.user_id,
+                count(transaction_id) as orders_3_days
+         from transactions t
+                  left join earliest e on t.user_id = e.user_id
+         where date_part('day', t.transaction_ts - e.earliest_order) * 24 +
+               DATE_PART('hour', t.transaction_ts - e.earliest_order) < 72
+         group by t.user_id) no_of_orders;
+         
