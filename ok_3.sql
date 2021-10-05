9f1f5d16329ed65c03fbff94e6a5d592502564ba
dmesitov <77168986+dmesitov@users.noreply.github.com>
2021-10-05 20:54:07 +0300

    Update ok_3.sql

--- ok_3.sql
+++ ok_3.sql
@@ -1,3 +1,4 @@
+--(a)	Выведи для каждого пользователя первое наименование, которое он заказал (первое по времени транзакции)
 select user_id,
        item
 from (
@@ -9,6 +10,8 @@ from (
 where seq = 1;
 
 
+
+--(b)	Посчитай сколько транзакций в среднем делает каждый пользователь в течении 72х часов с момента первой транзакции
 --explain analyse verbose
 with earliest as (
     select user_id,
@@ -23,5 +26,4 @@ from (
                   left join earliest e on t.user_id = e.user_id
          where date_part('day', t.transaction_ts - e.earliest_order) * 24 +
                DATE_PART('hour', t.transaction_ts - e.earliest_order) < 72
-         group by t.user_id) no_of_orders;
-         
+         group by t.user_id) no_of_orders;      
