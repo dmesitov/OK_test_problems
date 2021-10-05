b43b6ecadc88405383b74537a1c664d3d23f388e
dmesitov <77168986+dmesitov@users.noreply.github.com>
2021-10-05 20:52:58 +0300

    Update ok_2.sql

--- ok_2.sql
+++ ok_2.sql
@@ -1,24 +1,19 @@
+--(a)	Посчитай доход с женской аудитории (доход = сумма price * items)
+
 select sum(items * price) as income
 from purchases
 where left(user_gender, 1) = 'f';
 
 
+--(b)	Сравни доход по группе мужчин и женщин
 
 select left(user_gender, 1) as gender,
        sum(items * price) as income
 from purchases
 group by left(user_gender, 1);
 
--- select gender,
---        sum(income)
--- from (
---          select user_id,
---                 left(user_gender, 1) as gender,
---                 items * price        as income
---          from purchases
---      ) as incomes
--- group by gender;
 
+--(c)	Посчитай кол-во уникальных пользователей-мужчин, заказавших  более чем три айтема  (суммарно за все заказы)
 
 select count(user_id) as answer
 from (
